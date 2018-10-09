af70cc72abbafc4b78fa14b051cb384ef7469720
Megan Farley <megfarley4@gmail.com>
2018-10-09 17:40:39 -0500

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,11 +6,21 @@ def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
   # NOTE: in this demonstration we name many of the variables _hash or _array. 
   # This is done for educational purposes. This is not typically done in code.
-
-
+ 
+  char_hash = response_hash["results"].find { |data| data["name"].downcase == character} 
+#binding.pry
+  url_array = char_hash["films"]
+  #binding.pry
+  
+  film_array = url_array.collect do |url|
+    resp_string = RestClient.get(url)
+    resp_hash = JSON.parse(resp_string)
+  end
+  
+  #binding.pry
+  
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -22,7 +32,14 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
-def print_movies(films_hash)
+
+  #iterate iver film array and get movie titles
+
+
+def print_movies(movie_array)
+  movie_array.collect do |film|
+    puts film["title"]
+  end
   # some iteration magic and puts out the movies in a nice list
 end
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
   # puts out a welcome message here!
+  puts "hi"
 end
 
 def get_character_from_user
   puts "please enter a character"
+  input = gets.chomp
+  input.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
